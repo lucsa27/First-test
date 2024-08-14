3bf6dac02afeb7001aa1467cced6985c9b61902b
lucsa27 <123305959+lucsa27@users.noreply.github.com>
2024-08-14 17:17:50 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>“A vida não é um problema a ser resolvido, mas uma realidade a ser experimentada.” </h1>
+    return <h1>A felicidade da sua vida depende da qualidade dos seus pensamentos." – Marco Aurélio
+
+    <strong>Explicação:</strong>Marco Aurélio, em suas "Meditações", enfatiza que nossos pensamentos moldam nossa realidade e nossa felicidade. Ele sugere que cultivar pensamentos virtuosos e positivos é essencial para uma vida feliz e plena, alinhada com os princípios estóicos.</h1>
 
 }
 
