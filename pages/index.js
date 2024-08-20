f7501a26bf543fc3fae3572135d839b30e4e771c
lucsa27 <123305959+lucsa27@users.noreply.github.com>
2024-08-20 16:41:36 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 function Home() {
-    return (
-        <>
-            <h1>A felicidade da sua vida depende da qualidade dos seus pensamentos." – Marco Aurélio</h1>
-            <p><strong>Explicação:</strong> Marco Aurélio, em suas "Meditações", enfatiza que nossos pensamentos moldam nossa realidade e nossa felicidade. Ele sugere que cultivar pensamentos virtuosos e positivos é essencial para uma vida feliz e plena, alinhada com os princípios estóicos.</p>
-        </>
-    );
+  return (
+    <>
+      <h1>
+        A felicidade da sua vida depende da qualidade dos seus pensamentos." –
+        Marco Aurélio
+      </h1>
+      <p>
+        <strong>Explicação:</strong> Marco Aurélio, em suas "Meditações",
+        enfatiza que nossos pensamentos moldam nossa realidade e nossa
+        felicidade. Ele sugere que cultivar pensamentos virtuosos e positivos é
+        essencial para uma vida feliz e plena, alinhada com os princípios
+        estóicos.
+      </p>
+    </>
+  );
+
+  function teste() {
+    console.log("Identificado");
+  }
 }
 
 export default Home;
